32620c296fd3479d2c7ebb430f5f1d70f82e54af
Daniel X. Moore <yahivin@gmail.com>
2011-09-29 17:53:22 -0400

    pixieCanvas

--- browserlib.js
+++ browserlib.js
@@ -493,6 +493,604 @@ Music = (function() {
 })();;
 var __slice = Array.prototype.slice;
 (function($) {
+  return $.fn.pixieCanvas = function(options) {
+    var $canvas, canvas, context, contextAttrAccessor;
+    options || (options = {});
+    canvas = this.get(0);
+    context = void 0;
+    /**
+    PixieCanvas provides a convenient wrapper for working with Context2d.
+
+    Methods try to be as flexible as possible as to what arguments they take.
+
+    Non-getter methods return `this` for method chaining.
+
+    @name PixieCanvas
+    @constructor
+    */
+    $canvas = $(canvas).extend((function() {
+      /**
+      Passes this canvas to the block with the given matrix transformation
+      applied. All drawing methods called within the block will draw
+      into the canvas with the transformation applied. The transformation
+      is removed at the end of the block, even if the block throws an error.
+
+      @name withTransform
+      @methodOf PixieCanvas#
+
+      @param {Matrix} matrix
+      @param {Function} block
+
+      @returns this
+      */
+    })(), {
+      withTransform: function(matrix, block) {
+        context.save();
+        context.transform(matrix.a, matrix.b, matrix.c, matrix.d, matrix.tx, matrix.ty);
+        try {
+          block(this);
+        } finally {
+          context.restore();
+        }
+        return this;
+      }
+    }, (function() {
+      /**
+      Clear the canvas (or a portion of it).
+
+      Clear the entire canvas
+
+      <code><pre>
+      canvas.clear()
+      </code></pre>
+
+      Clear a portion of the canvas
+
+      <code class="run"><pre>
+      # Set up: Fill canvas with blue
+      canvas.fill("blue")  
+
+      # Clear a portion of the canvas
+      canvas.clear
+        x: 50
+        y: 50
+        width: 50
+        height: 50
+      </pre></code>
+
+      You can also clear the canvas by passing x, y, width height as
+      unnamed parameters:
+
+      <code><pre>
+      canvas.clear(25, 25, 50, 50)
+      </code></pre>
+
+      @name clear
+      @methodOf PixieCanvas#
+
+      @param {Number} [x]
+      @param {Number} [y]
+      @param {Number} [width]
+      @param {Number} [height]
+
+      @returns this
+      */
+    })(), {
+      clear: function(x, y, width, height) {
+        var _ref;
+        if (x == null) {
+          x = {};
+        }
+        if (y == null) {
+          _ref = x, x = _ref.x, y = _ref.y, width = _ref.width, height = _ref.height;
+        }
+        x || (x = 0);
+        y || (y = 0);
+        if (width == null) {
+          width = canvas.width;
+        }
+        if (height == null) {
+          height = canvas.height;
+        }
+        context.clearRect(x, y, width, height);
+        return this;
+      }
+    }, (function() {
+      /**
+      Fills the entire canvas (or a specified section of it) with
+      the given color.
+
+      <code class="run"><pre>
+      # Paint the town (entire canvas) red
+      canvas.fill "red"
+
+      # Fill a section of the canvas white (#FFF)
+      canvas.fill
+        x: 50
+        y: 50
+        width: 50
+        height: 50
+        color: "#FFF"
+      </pre></code>
+
+      @name fill
+      @methodOf PixieCanvas
+
+      @param {Number} [x=0] Optional x position to fill from.
+      @param {Number} [y=0]
+      @param {Number} [width=canvas.width]
+      @param {Number} [height=canvas.height]
+      @param {Bounds} [bounds]
+      @param {String|Color} [color]
+
+      @returns this
+      */
+    })(), {
+      fill: function(color) {
+        var bounds, height, width, x, y, _ref;
+        if (color == null) {
+          color = {};
+        }
+        if (!(typeof color.isString === "function" ? color.isString() : void 0)) {
+          _ref = color, x = _ref.x, y = _ref.y, width = _ref.width, height = _ref.height, bounds = _ref.bounds, color = _ref.color;
+        }
+        if (bounds) {
+          x = bounds.x, y = bounds.y, width = bounds.width, height = bounds.height;
+        }
+        x || (x = 0);
+        y || (y = 0);
+        if (width == null) {
+          width = canvas.width;
+        }
+        if (height == null) {
+          height = canvas.height;
+        }
+        this.fillColor(color);
+        context.fillRect(x, y, width, height);
+        return this;
+      }
+    }, (function() {
+      /**
+      This is a gnarly function. Your best bet is to let Drawable handle it. 
+
+      @name drawImage
+      @methodOf PixieCanvas#
+
+      @returns this
+      */
+    })(), {
+      drawImage: function(image, sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight) {
+        context.drawImage(image, sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight);
+        return this;
+      }
+    }, (function() {
+      /**
+      Draws a circle at the specified position with the specified
+      radius and color.
+
+      <code class="run"><pre>
+      # Draw a large orange circle
+      canvas.drawCircle
+        radius: 30
+        position: Point(100, 75)
+        color: "orange"
+
+      # Draw a blue circle with radius 10 at (25, 50)
+      # and a red stroke
+      canvas.drawCircle
+        x: 25
+        y: 50
+        raduis: 10
+        color: "blue"
+        stroke:
+          color: "red"
+          width: 1
+
+      # Create a circle object to set up the next examples
+      circle =
+        radius: 20
+        x: 50
+        y: 50
+
+      # Draw a given circle in yellow
+      canvas.drawCircle
+        circle: circle
+        color: "yellow"
+
+      # Draw the circle in green at a different position
+      canvas.drawCircle
+        circle: circle
+        position: Point(25, 75)
+        color: "green"
+
+      # Draw an outline circle in purple.
+      canvas.drawCircle
+        x: 50
+        y: 75
+        radius: 10
+        stroke:
+          color: "purple"
+          width: 2
+      </pre></code>
+
+      @name drawCircle
+      @methodOf PixieCanvas#
+
+      @param {Number} x
+      @param {Number} y
+      @param {Point} [position]
+      @param {Number} radius
+      @param {Color|String} [color]
+      @param {Circle} [circle]
+
+      @returns this
+      */
+    })(), {
+      drawCircle: function(_arg) {
+        var circle, color, position, radius, stroke, x, y;
+        x = _arg.x, y = _arg.y, radius = _arg.radius, position = _arg.position, color = _arg.color, stroke = _arg.stroke, circle = _arg.circle;
+        if (circle) {
+          x = circle.x, y = circle.y, radius = circle.radius;
+        }
+        if (position) {
+          x = position.x, y = position.y;
+        }
+        context.beginPath();
+        context.arc(x, y, radius, 0, Math.TAU, true);
+        context.closePath();
+        if (color) {
+          this.fillColor(color);
+          context.fill();
+        }
+        if (stroke) {
+          this.strokeColor(stroke.color);
+          this.lineWidth(stroke.width);
+          context.stroke();
+        }
+        return this;
+      }
+    }, (function() {
+      /**
+      Draws a rectangle at the specified position with given 
+      width and height. Optionally takes a position, bounds
+      and color argument.
+
+      <code class="run"><pre>
+      # Draw a red rectangle using x, y, width and height
+      canvas.drawRect
+        x: 50
+        y: 50
+        width: 50
+        height: 50
+        color: "#F00"
+
+      # Draw a blue rectangle using position, width and height
+      # and throw in a stroke for good measure
+      canvas.drawRect
+        position: Point(0, 0)
+        width: 50
+        height: 50
+        color: "blue"
+        stroke:
+          color: "orange"
+          width: 3
+
+      # Set up a bounds object for the next examples
+      bounds =
+        x: 100
+        y: 0
+        width: 100
+        height: 100
+
+      # Draw a purple rectangle using bounds
+      canvas.drawRect
+        bounds: bounds
+        color: "green"
+
+      # Draw the outline of the same bounds, but at a different position
+      canvas.drawRect
+        bounds: bounds
+        position: Point(0, 50)
+        stroke:
+          color: "purple"
+          width: 2
+      </pre></code>
+
+      @name drawRect
+      @methodOf PixieCanvas#
+
+      @param {Number} [x]
+      @param {Number} [y]
+      @param {Number} [width]
+      @param {Number} [height]
+      @param {Point} [position]
+      @param {Color|String} [color]
+      @param {Bounds} [bounds]
+      @param {Stroke} [stroke]
+
+      @returns this
+      */
+    })(), {
+      drawRect: function(_arg) {
+        var bounds, color, height, position, stroke, width, x, y;
+        x = _arg.x, y = _arg.y, width = _arg.width, height = _arg.height, position = _arg.position, bounds = _arg.bounds, color = _arg.color, stroke = _arg.stroke;
+        if (bounds) {
+          x = bounds.x, y = bounds.y, width = bounds.width, height = bounds.height;
+        }
+        if (position) {
+          x = position.x, y = position.y;
+        }
+        if (color) {
+          this.fillColor(color);
+          context.fillRect(x, y, width, height);
+        }
+        if (stroke) {
+          this.strokeColor(stroke.color);
+          this.lineWidth(stroke.width);
+          context.strokeRect(x, y, width, height);
+        }
+        return this;
+      }
+    }, (function() {
+      /**
+      Draw a line from `start` to `end`.
+
+      <code class="run"><pre>
+      # Draw a sweet diagonal
+      canvas.drawLine
+        start: Point(0, 0)
+        end: Point(200, 200)
+        color: "purple"
+
+      # Draw another sweet diagonal
+      canvas.drawLine
+        start: Point(200, 0)
+        end: Point(0, 200)
+        color: "red"
+        width: 6
+
+      # Now draw a sweet horizontal with a direction and a length
+      canvas.drawLine
+        start: Point(0, 100)
+        length: 200
+        direction: Point(1, 0)
+        color: "orange"
+
+      </pre></code>
+
+      @name drawLine
+      @methodOf PixieCanvas#
+
+      @param {Point} start
+      @param {Point} [end]
+      @param {Number} [width]
+      @param {String|Color} [color]
+
+      @returns this
+      */
+    })(), {
+      drawLine: function(_arg) {
+        var color, direction, end, length, start, width;
+        start = _arg.start, end = _arg.end, width = _arg.width, color = _arg.color, direction = _arg.direction, length = _arg.length;
+        width || (width = 3);
+        if (direction) {
+          end = direction.norm(length).add(start);
+        }
+        this.lineWidth(width);
+        this.strokeColor(color);
+        context.beginPath();
+        context.moveTo(start.x, start.y);
+        context.lineTo(end.x, end.y);
+        context.closePath();
+        context.stroke();
+        return this;
+      }
+    }, (function() {
+      /**
+      @name drawPoly
+      @methodOf PixieCanvas#
+
+      @returns this
+      */
+    })(), {
+      drawPoly: function(_arg) {
+        var color, points, stroke;
+        points = _arg.points, color = _arg.color, stroke = _arg.stroke;
+        context.beginPath();
+        points.each(function(point, i) {
+          if (i === 0) {
+            return context.moveTo(point.x, point.y);
+          } else {
+            return context.lineTo(point.x, point.y);
+          }
+        });
+        context.lineTo(points[0].x, points[0].y);
+        if (color) {
+          this.fillColor(color);
+          context.fill();
+        }
+        if (stroke) {
+          this.strokeColor(stroke.color);
+          this.lineWidth(stroke.width);
+          context.stroke();
+        }
+        return this;
+      }
+    }, (function() {
+      /**
+      Draw a rounded rectangle.
+
+      Adapted from http://js-bits.blogspot.com/2010/07/canvas-rounded-corner-rectangles.html
+
+      @param {Number} [x]
+      @param {Number} [y]
+      @param {Number} [width]
+      @param {Number} [height]
+      @param {Number} [radius] Defaults to 5
+      @param {Point} [position]
+      @param {Color|String} [color]
+      @param {Bounds} [bounds]
+      @param {Stroke} [stroke]
+
+      @returns this
+      */
+    })(), {
+      drawRoundRect: function(_arg) {
+        var bounds, color, height, position, radius, stroke, width, x, y;
+        x = _arg.x, y = _arg.y, width = _arg.width, height = _arg.height, radius = _arg.radius, position = _arg.position, bounds = _arg.bounds, color = _arg.color, stroke = _arg.stroke;
+        if (radius == null) {
+          radius = 5;
+        }
+        if (bounds) {
+          x = bounds.x, y = bounds.y, width = bounds.width, height = bounds.height;
+        }
+        if (position) {
+          x = position.x, y = position.y;
+        }
+        context.beginPath();
+        context.moveTo(x + radius, y);
+        context.lineTo(x + width - radius, y);
+        context.quadraticCurveTo(x + width, y, x + width, y + radius);
+        context.lineTo(x + width, y + height - radius);
+        context.quadraticCurveTo(x + width, y + height, x + width - radius, y + height);
+        context.lineTo(x + radius, y + height);
+        context.quadraticCurveTo(x, y + height, x, y + height - radius);
+        context.lineTo(x, y + radius);
+        context.quadraticCurveTo(x, y, x + radius, y);
+        context.closePath();
+        if (color) {
+          this.fillColor(color);
+          context.fill();
+        }
+        if (stroke) {
+          this.lineWidth(stroke.width);
+          this.strokeColor(stroke.color);
+          context.stroke();
+        }
+        return this;
+      }
+    }, (function() {
+      /**
+      Draws text on the canvas at the given position, in the given color.
+      If no color is given then the previous fill color is used.
+
+      @name drawText
+      @methodOf PixieCanvas#
+
+      @param {Number} [x]
+      @param {Number} [y]
+      @param {String} text
+      @param {Point} [position]
+      @param {String|Color} [color]
+      */
+    })(), {
+      drawText: function(_arg) {
+        var color, position, text, x, y;
+        x = _arg.x, y = _arg.y, text = _arg.text, position = _arg.position, color = _arg.color;
+        if (position) {
+          x = position.x, y = position.y;
+        }
+        this.fillColor(color);
+        context.fillText(text, x, y);
+        return this;
+      }
+    }, (function() {
+      /**
+      Centers the given text on the canvas at the given y position. An x position
+      or point position can also be given in which case the text is centered at the
+      x, y or position value specified.
+
+      @name centerText
+      @methodOf PixieCanvas#
+
+      @param {String} text
+      @param {Number} y
+      @param {Number} [x]
+      @param {Point} [position]
+      @param {String|Color} [color]
+      */
+    })(), {
+      centerText: function(_arg) {
+        var color, position, text, textWidth, x, y;
+        text = _arg.text, x = _arg.x, y = _arg.y, position = _arg.position, color = _arg.color;
+        if (position) {
+          x = position.x, y = position.y;
+        }
+        if (x == null) {
+          x = canvas.width / 2;
+        }
+        textWidth = this.measureText(text);
+        return this.fillText({
+          text: text,
+          color: color,
+          x: x - textWidth / 2,
+          y: y
+        });
+      },
+      fillColor: function(color) {
+        if (color) {
+          if (color.channels) {
+            context.fillStyle = color.toString();
+          } else {
+            context.fillStyle = color;
+          }
+          return this;
+        } else {
+          return context.fillStyle;
+        }
+      },
+      strokeColor: function(color) {
+        if (color) {
+          if (color.channels) {
+            context.strokeStyle = color.toString();
+          } else {
+            context.strokeStyle = color;
+          }
+          return this;
+        } else {
+          return context.strokeStyle;
+        }
+      },
+      measureText: function(text) {
+        return context.measureText(text).width;
+      },
+      putImageData: function(imageData, x, y) {
+        context.putImageData(imageData, x, y);
+        return this;
+      },
+      context: function() {
+        return context;
+      },
+      element: function() {
+        return canvas;
+      }
+    });
+    contextAttrAccessor = function() {
+      var attrs;
+      attrs = 1 <= arguments.length ? __slice.call(arguments, 0) : [];
+      return attrs.each(function(attr) {
+        return $canvas[attr] = function(newVal) {
+          if (newVal != null) {
+            context[attr] = newVal;
+            return this;
+          } else {
+            return context[attr];
+          }
+        };
+      });
+    };
+    contextAttrAccessor("font", "globalAlpha", "globalCompositeOperation", "height", "lineWidth", "textAlign", "width");
+    if (canvas != null ? canvas.getContext : void 0) {
+      context = canvas.getContext('2d');
+      if (options.init) {
+        options.init($canvas);
+      }
+      return $canvas;
+    }
+  };
+})(jQuery);;
+var __slice = Array.prototype.slice;
+(function($) {
   return $.fn.powerCanvas = function(options) {
     var $canvas, canvas, context;
     options || (options = {});
