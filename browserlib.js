3b3a4410cf17b9b8f3f652a8c0fd962dd4c4f6a6
Daniel X. Moore <yahivin@gmail.com>
2011-09-29 23:54:33 -0400

    Modified in browser at pixie.strd6.com

--- browserlib.js
+++ browserlib.js
@@ -900,8 +900,29 @@ var __slice = Array.prototype.slice;
       }
     }, (function() {
       /**
+      Draw a polygon.
+
+      <code class="run"><pre>
+      # Draw a sweet rhombus
+      canvas.drawPoly
+        points: [
+          Point(50, 25)
+          Point(75, 50)
+          Point(50, 75)
+          Point(25, 50)
+        ]
+        color: "purple"
+        stroke:
+          color: "red"
+          width: 2
+      </pre></code>
+
       @name drawPoly
       @methodOf PixieCanvas#
+
+      @param {Point[]} [points]
+      @param {String|Color} [color]
+      @param {stroke} [stroke]
 
       @returns this
       */
