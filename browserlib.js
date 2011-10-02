8a60fb6a0657239c653f5036508adc8721491095
Daniel X. Moore <yahivin@gmail.com>
2011-10-02 18:20:55 -0400

    Modified in browser at pixie.strd6.com

--- browserlib.js
+++ browserlib.js
@@ -1576,7 +1576,17 @@ var __slice = Array.prototype.slice;
     }
   };
 })(jQuery);;
-window.requestAnimationFrame || (window.requestAnimationFrame = window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame || function(callback, element) {
+/**
+A browser polyfill so you can consistently 
+call requestAnimationFrame. Using 
+requestAnimationFrame is preferred to 
+setInterval for main game loops.
+
+http://paulirish.com/2011/requestanimationframe-for-smart-animating/
+
+@name requestAnimationFrame
+@namespace
+*/window.requestAnimationFrame || (window.requestAnimationFrame = window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame || function(callback, element) {
   return window.setTimeout(function() {
     return callback(+new Date());
   }, 1000 / 60);
